11c0388ad8ce9a68c6a18c32e4a313f48baebb0c
ShanSenanayake <senanayake92@gmail.com>
2015-05-20 09:51:50 +0200

    can calulate best possible splitpoints

--- decision_trees/EDAN70.java
+++ decision_trees/EDAN70.java
@@ -9,27 +9,45 @@ public class EDAN70 {
 		DecisionTreeParser dtp = new DecisionTreeParser(new File(args[0]),
 				args[1]);
 		Relation rel = dtp.getRelation();
-		Scanner scan = new Scanner(System.in);
-		for (int i = 0; i < rel.getAttributes().size(); i++) {
-			Attribute attr = rel.getAttributes().get(i);
-			if (!attr.getValues().contains("%numeric")){
-				continue;
-			}
-			System.out.println("Input splitpoint for " + attr.toString() + ": ");
-			attr.setSplitPoint(scan.nextDouble());
-		}
-		scan.close();
-		System.out.println("Calculating...");
 		DecisionTreeAlgorithm dta = new DecisionTreeAlgorithm(rel);
+
+		for (Attribute attr : rel.getAttributes()) {
+			ArrayList<Pair> list = new ArrayList<Pair>();
+			for (Example exam : rel.getExamples()) {
+				Goal goal = exam.getGoal();
+				Pair p = new Pair(Double.parseDouble(exam.getValue(attr)),
+						goal.getClassification());
+				list.add(p);
+			}
+			Collections.sort(list);
+			double bestSplit = -1;
+			double info = -1;
+			Pair prev = list.get(0);
+			for (Pair curr : list) {
+
+				if (prev.getClassification() != curr.getClassification()) {
+
+					double currentSplit = (prev.getValue() + curr.getValue()) / 2;
+
+					attr.setSplitPoint(currentSplit);
+
+					double temp = dta.calculateGain(attr, rel.getExamples());
+
+					if (temp > info) {
+						bestSplit = currentSplit;
+						info = temp;
+					}
+				}
+				prev = curr;
+			}
+			attr.setSplitPoint(bestSplit);
+		}
+
 		DecisionNode node = dta.dtl();
-		System.out.println("Finished!");
-		if (args.length >= 3 && args[2].equals("prune")){
-			System.out.println("Pruning...");
+		if (args.length >= 3 && args[2].equals("prune")) {
 			node = dta.pruning(node);
-			System.out.println("Finished!");
 		}
 		System.out.println(node.print(0));
-
 	}
 
 }
